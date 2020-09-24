9f706a44b9c99f2de2740553b5248a15c1cc5030
caomei4321 <43211374@qq.com>
2020-09-24 11:01:44 +0800

    删除冗余

--- routes/api.php
+++ routes/api.php
@@ -94,61 +94,14 @@ $api->version('v1', [
         $api->get('repairDetail/{id}', 'RepairsController@repairDetail');  // 报修详情
 
         $api->post('completeRepair', 'RepairsController@completeRepair'); //完成报修
-
-        $api->get('userHasMatters', 'MattersController@userHasMatters'); //用户未完成任务
-
-        $api->get('userCompleteMatters', 'MattersController@userCompleteMatters'); //用户已完成任务
-
-        $api->get('userMatters', 'MattersController@userMatters'); //用户全部任务
-
-        $api->get('matter', 'MattersController@matter');   //任务详情
-
-        //$api->get('categories', 'CategoryController@categories');  //责任清单分类
-
-        $api->post('endMatter', 'MattersController@endImportMatter');   //完成12345任务
-
-        $api->get('patrolList', 'PatrolController@patrolList');   //巡查记录
-
-        $api->post('importMatter', 'MattersController@findMatterAndEnd');  //发现并提交问题
-
-        $api->get('patrolMatters', 'MattersController@patrolMatters'); //用户巡查中上报的全部任务
-
-        $api->post('startAndEndPatrol', 'PatrolController@startAndEndPatrol');  //开始和结束巡逻
-
-        // 警报 alarm
-        $api->get('userHasAlarms', 'AlarmsController@userHasAlarms');   // 告警任务派发列表
-        $api->post('completeAlarm', 'AlarmsController@completeAlarm');  // 告警完成
     });
-
-    $api->post('alarm', 'AlarmsController@alarm');  // 摄像头告警
-
-    $api->post('helmetAlarm', 'HatsController@helmetAlarm'); // 安全帽检测
-
-    $api->get('carouselMap', 'ProgramImagesController@carouselMap');
-
-    // 公开隐藏
-    $api->get('openMatters', 'ProgramImagesController@matters');
-    // 要闻信息
-    $api->get('news', 'NewsController@index');
-    // 详情
-    $api->get('newsDetail/{id}', 'NewsController@newsDetail');
-
     // 上传版本
     $api->post('fileUpload','FileUploadController@save')->name('api.fileUpload.save');
 
-    $api->get('version','VersionsController@version');
 
-    $api->post('zr','VersionsController@zr');
 
     $api->group(['middleware' => 'auth:programApi'], function ($api) {
-        $api->post('matterStore', 'ProgramUsersController@matterStore');  // 上报问题
-        $api->post('matterStoreInfo', 'ProgramUsersController@matterStoreInfo'); // 上报问题信息
-        $api->post('matterStoreImage', 'ProgramUsersController@matterStoreImage'); // 上报问题图片
-        $api->get('historyMatters', 'ProgramUsersController@historyMatters');  //历史提交问题记录
-        $api->get('historyMattersDetail/{id}', 'ProgramUsersController@historyMattersDetail'); // 历史提交问题详情
-        $api->get('weappUser', 'ProgramUsersController@weappUser');  // 当前用户信息
-        $api->post('newsComment', 'NewsController@comment'); // 评论
-        $api->post('destroyComment', 'NewsController@destroyComment'); // 删除评论
+
     });
 
 
